1df43cb8bf855278d6f2c0b05f8b627480c6437d
LettischesFerkel <ksvaza10@gmail.com>
2024-09-17 14:13:25 +0300

    modified:   .vscode/settings.json 	modified:   code/main.c

--- code/main.c
+++ code/main.c
@@ -29,9 +29,26 @@ int init()
     clearScreen(64);
 }
 
+calculate()
+{
+    int result = 0;
+    for(int y = 0; y < windowHeight; y++)
+    {
+        for(int x = 0; x < windowWidth-1; x++)
+        {
+            result = 0;
+            result = (((x - y) + 0.5) >= 0) && (((x - y) - 0.5) <= 0);
+            window[y][x] = result * 64;
+        }
+        window[y][windowWidth-1] = '\n';
+        if(y == windowHeight-1) { window[windowHeight-1][windowWidth-1] = '\0'; }
+    }
+}
+
 int update()
 {
     clearScreen(64);
+    calculate();
     drawScreen();
     fgets(&output[0], 64, stdin);
     running = 0;
