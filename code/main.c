602d3b2ffcbcaa28058b31c24cefa06fe1812f0d
LettischesFerkel <ksvaza10@gmail.com>
2024-09-17 14:09:00 +0300

    modified:   .vscode/launch.json 	modified:   code/main.c

--- code/main.c
+++ code/main.c
@@ -1,7 +1,51 @@
 #include <stdio.h>
 
+int running = 1;
+
+#define windowWidth 208
+#define windowHeight 54
+char window[windowHeight][windowWidth];
+
+char output[65];
+
+int drawScreen()
+{
+    printf("\n--------------------------------\n%s\n--------------------------------\n", window);
+}
+int clearScreen(char filler)
+{
+    for(int y = 0; y < windowHeight; y++)
+    {
+        for(int x = 0; x < windowWidth-1; x++)
+        {
+            window[y][x] = filler;
+        }
+        window[y][windowWidth-1] = '\n';
+        if(y == windowHeight-1) { window[windowHeight-1][windowWidth-1] = '\0'; }
+    }
+}
+int init()
+{
+    clearScreen(64);
+}
+
+int update()
+{
+    clearScreen(64);
+    drawScreen();
+    fgets(&output[0], 64, stdin);
+    running = 0;
+}
+
 int main()
 {
-    printf("Amogus is sus %c", '\n');
+    printf("Amogus is sus\n");
+
+    init();
+    while(running)
+    {
+        update();
+    }
+
     return 1;
 }
