b1c2aa75b7f0a777aff577ade23f0357915d9e49
LettischesFerkel <ksvaza10@gmail.com>
2024-09-27 15:21:19 +0300

    jaunas izmaiņas

--- code/main.c
+++ code/main.c
@@ -23,6 +23,10 @@ SDL_Rect windowDimensions;
 int keep_window_open = 1;
 
 SDL_Surface* image;
+SDL_Surface* imageZils;
+SDL_Surface* imageDzeltens;
+SDL_Surface* imageBalts;
+SDL_Surface* imageSarkans;
 
 SDL_Rect imageDimensions;
 
@@ -33,21 +37,55 @@ typedef struct {
     double vy;
     double w;
     double h;
+    double d;
 }particle;
-
+typedef struct {
+    float x;
+    float y;
+}vector2f;
+const vector2f nullVector2f = {0, 0};
 #define partikles 10
-double gravitate = -1;
+
+const double gravitate = -10;
+const float temperatura = 100; // pikseļos/sekundē obv
+
 particle maksimilianaKungs[partikles]; // partikāļi
-particle imageDefParticle = { 0, 0, 0, 0, 200, 200 };
-int start()
+particle imageDefParticle = { 0, 0, 0, 0, 200, 200, 200 };
+vector2f imageSize = {200, 200};
+
+float vektorMagnitude(vector2f vektor)
 {
-    image = SDL_LoadBMP("res/image02.bmp");
-    if(!image)
+    return sqrt(pow(vektor.x, 2) + pow(vektor.y, 2));
+}
+vector2f normaliseVektor(vector2f unnormalVecotr, float desiredMagnitude)
+{
+    float magnitude = vektorMagnitude(unnormalVecotr);
+    if (magnitude == 0) { return nullVector2f; }
+    float relativeMagnitude = desiredMagnitude / magnitude;
+    vector2f result;
+    result.x = unnormalVecotr.x * relativeMagnitude;
+    result.y = unnormalVecotr.y * relativeMagnitude;
+    return result;
+}
+SDL_Surface* loadBMPImage(char* adress)
+{
+    SDL_Surface* img = SDL_LoadBMP(adress);
+    if(!img)
     {
         printf("Failed to load image\n");
         printf("SDL2 Error: %d\n",  SDL_GetError());
-        return -1;
+        return NULL;
     }
+    return img;
+}
+
+int start()
+{
+    image = loadBMPImage("res/zalsApalsStarpMums.bmp");
+    imageZils = loadBMPImage("res/zilsApalsStarpMums.bmp");
+    imageDzeltens = loadBMPImage("res/dzeltensApalsStarpMums.bmp");
+    imageBalts = loadBMPImage("res/baltsApalsStarpMums.bmp");
+    imageSarkans = loadBMPImage("res/sarkansApalsStarpMums.bmp");
     imageDefParticle.x = (windowDimensions.w - imageDefParticle.w) / 2;
     imageDefParticle.y = (windowDimensions.h - imageDefParticle.h) / 2; // neticēsi
 
@@ -57,7 +95,20 @@ int start()
         maksimilianaKungs[i].y = imageDefParticle.y;
         maksimilianaKungs[i].w = imageDefParticle.w;
         maksimilianaKungs[i].h = imageDefParticle.h;
+        maksimilianaKungs[i].d = imageDefParticle.d;
     }
+
+    vector2f normalisationVector;
+    vector2f randomisationVector;
+    for (int i = 1; i < partikles; i++) { 
+        maksimilianaKungs[i].x = (rand() % (int)(windowDimensions.w - maksimilianaKungs[i].w + 1)) + maksimilianaKungs[i].w / 2;
+        maksimilianaKungs[i].y = (rand() % (int)(windowDimensions.h - maksimilianaKungs[i].h + 1)) + maksimilianaKungs[i].h / 2;
+        randomisationVector.x = (rand() % (100 + 1)) - 50;
+        randomisationVector.y = (rand() % (100 + 1)) - 50;
+        normalisationVector = normaliseVektor(randomisationVector, temperatura);
+        maksimilianaKungs[i].vx = normalisationVector.x;
+        maksimilianaKungs[i].vy = normalisationVector.y;
+    } 
 }
 
 int physicsUpdate(double deltaTime)
@@ -71,26 +122,26 @@ int physicsUpdate(double deltaTime)
         maksimilianaKungs[i].y = maksimilianaKungs[i].y + (maksimilianaKungs[i].vy * deltaTime);
 
         // faktiski apspiest maksimiliana kunga apkārtmerus un parvietošanās iespējas ekrāna ietvaros
-        if (maksimilianaKungs[i].y >= windowDimensions.h - maksimilianaKungs[i].h)
+        if (maksimilianaKungs[i].y > windowDimensions.h - maksimilianaKungs[i].h)
         {
             maksimilianaKungs[i].y = windowDimensions.h - maksimilianaKungs[i].h;
-            maksimilianaKungs[i].vy = 0;
+            maksimilianaKungs[i].vy = -maksimilianaKungs[i].vy;
         }
-        else if (maksimilianaKungs[i].y <= 0 + maksimilianaKungs[i].h)
+        else if (maksimilianaKungs[i].y < 0)
         {
-            maksimilianaKungs[i].y = 0 + maksimilianaKungs[i].h;
-            maksimilianaKungs[i].vy = 0;
+            maksimilianaKungs[i].y = 0;
+            maksimilianaKungs[i].vy = -maksimilianaKungs[i].vy;
         }
         
-        if (maksimilianaKungs[i].x >= windowDimensions.w - maksimilianaKungs[i].w)
+        if (maksimilianaKungs[i].x > windowDimensions.w - maksimilianaKungs[i].w)
         {
             maksimilianaKungs[i].x = windowDimensions.w - maksimilianaKungs[i].w;
-            maksimilianaKungs[i].vx = 0;
+            maksimilianaKungs[i].vx = -maksimilianaKungs[i].vx;
         }
-        else if (maksimilianaKungs[i].x <= 0 + maksimilianaKungs[i].w)
+        else if (maksimilianaKungs[i].x < 0)
         {
-            maksimilianaKungs[i].x = 0 + maksimilianaKungs[i].w;
-            maksimilianaKungs[i].vx = 0;
+            maksimilianaKungs[i].x = 0;
+            maksimilianaKungs[i].vx = -maksimilianaKungs[i].vx;
         }
     }
 }
@@ -103,13 +154,29 @@ SDL_Rect partikleTuRekt(particle target)
     result.h = target.h;
     return result;
 }
+SDL_Rect vektorTuRekt(vector2f target)
+{
+    SDL_Rect result;
+    result.x = 0;
+    result.y = 0;
+    result.w = target.x;
+    result.h = target.y;
+    return result;
+}
 
 int update(double deltaTime)
 {
     //if (deltaTime != (double)0) { printf("Amogus = ( %6.1f, %6.1f )\n", maksimilianaKungs[0].x, maksimilianaKungs[0].y); }
     physicsUpdate(deltaTime);
-    SDL_Rect imgPrtcle = partikleTuRekt(maksimilianaKungs[0]);
-    SDL_BlitSurface(image, NULL, window_surface, &imgPrtcle);
+
+    SDL_Rect imgPrtcle;
+    SDL_Rect wndPrtclis;
+    for (int i = 0; i < partikles; i++)
+    {
+        imgPrtcle = partikleTuRekt(maksimilianaKungs[i]);
+        wndPrtclis = vektorTuRekt(imageSize);
+        SDL_BlitSurface(imageDzeltens, &wndPrtclis, window_surface, &imgPrtcle);
+    }
 }
 int processEvent(int eventType)
 {
@@ -127,15 +194,15 @@ int initWindow()
 {
     windowDimensions.x = 0;
     windowDimensions.y = 0;
-    windowDimensions.w = 720;
-    windowDimensions.h = 600;
+    windowDimensions.w = 1920;
+    windowDimensions.h = 1080;
     if(SDL_Init(SDL_INIT_VIDEO) < 0)
     {
         printf("Failed to initialize the SDL2 library\n");
         return -1;
     }
 
-    window = SDL_CreateWindow("Melnums", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 720, 600, 0);
+    window = SDL_CreateWindow("Melnums", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, windowDimensions.w, windowDimensions.h, 0);
 
     if(!window)
     {
@@ -173,8 +240,12 @@ int main(int argc, char *argv[])
     double elapsedTime = 0;
     QueryPerformanceFrequency(&frequency);
 
-    fgets(&inputBuffer[0], 64, stdin);
+    //fgets(&inputBuffer[0], 64, stdin);
     printf("The Sus has arised.\n\n");
+    vector2f test = {40, 40};
+    printf("vektor = (%f, %f) ; |vektor| = %f\n", test.x, test.y, vektorMagnitude(test));
+    test = normaliseVektor(test, 100);
+    printf("norm(vektor) = (%f, %f) ; |norm(vektor)| = %f\n\n", test.x, test.y, vektorMagnitude(test));
     initWindow();
     start();
 
